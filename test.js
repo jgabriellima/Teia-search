e53a057f756ceb9a764c943cdce7abae1179b1a1
unknown <GABRIEL.LIMA@DTI-118.sefa.pa.gov.br>
2015-09-21 11:42:21 -0300

    ajustes e melhorias na sintaxe e processamento

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 var teia = require("./teia-search.js");
-
 teia.init({
     base: "C:\\workspace_full_migration",
     restriction: "branch",
-    exclude:"target",
-    filefilter: "filter_migracao.txt",
-    fileResult: "result.txt",
+    exclude: "target",
+    filefilter: "filters/filter_migracao.txt",
+    fileResult: "results/result.txt",
     indexpath: 2
 });
 teia.debug(true);
